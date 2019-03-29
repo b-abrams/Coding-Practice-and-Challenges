d712d01c326074a780f9e270b38d3d5ce057777b
Breeland Abrams <breeland.abrams@gmail.com>
2019-03-28 20:58:19 -0400

    #35 Leetcode

---
+++ Challenges/Leetcode/35/Solution.java
@@ -0,0 +1,13 @@
+//Breeland Abrams
+//Leetcode #35 Insert Search Position
+class Solution {
+  public int searchInsert(int[] nums, int target) {
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] >= target) {
+        return i;
+      }
+    }
+    return nums.length;
+  }
+}
